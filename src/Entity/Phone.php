09d380f02d459c01f4503995b06ffe0bbc261898
Karim <abdounikarim@gmail.com>
2019-04-18 11:33:02 +0200

    data validation

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
@@ -21,12 +22,16 @@ class Phone
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"list", "show"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min="2", minMessage="Ce champ doit contenir un minimum de {{ limit }} caractères", max="255", maxMessage="Ce champ doit contenir un maximum de {{ limit }} caractères")
      */
     private $name;
 
     /**
      * @ORM\Column(type="integer")
      * @Groups({"list", "show"})
+     * @Assert\NotBlank()
+     * @Assert\Range(min="0", minMessage="La valeur minimum autorisée est {{ limit }}", max="1500", maxMessage="La valeur maximum autorisée est {{ limit }}")
      */
     private $price;
 
